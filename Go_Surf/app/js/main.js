7223d54bd2408fed7f4281c5fbe56c1d9a75362d
Lera Makoviy <valerya.makoviy@gmail.com>
2021-07-09 18:00:44 +0300

    Add site HEADER. HTML/CSS/Animation

--- Go_Surf/app/js/main.js
+++ Go_Surf/app/js/main.js
@@ -1,6 +1,15 @@
+
 $(function(){
-
-
-
-  
+    $('.header_slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: "<img class='slider-arrows slider-arrows_left' src=\"img/arrows-left.svg\" alt=\"\">",
+        nextArrow: "<img class='slider-arrows slider-arrows_right' src=\"img/arrows-right.svg\" alt=\"\">",
+        asNavFor: '.slider-dots',
+    });
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header_slider',
+    });
 });
